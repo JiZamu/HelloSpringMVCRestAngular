36c82a6c4ba78dca325e722f62fd1b99a65badba
Eduardo Jimenez <worldjiz@gmail.com>
2015-08-20 12:42:48 -0500

    Sin cambios importantes

--- src/main/java/com/whileapp/springmvcrestangular/controllers/rest/AppRestController.java
+++ src/main/java/com/whileapp/springmvcrestangular/controllers/rest/AppRestController.java
@@ -1,5 +1,8 @@
 package com.whileapp.springmvcrestangular.controllers.rest;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppRestController {
     
 }
