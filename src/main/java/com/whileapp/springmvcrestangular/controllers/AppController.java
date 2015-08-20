8ee175aca282cb1f709bf53a64334275d554db93
Eduardo Jimenez <worldjiz@gmail.com>
2015-08-20 11:56:38 -0500

    Funcionamiento AppController OK

--- src/main/java/com/whileapp/springmvcrestangular/controllers/AppController.java
+++ src/main/java/com/whileapp/springmvcrestangular/controllers/AppController.java
@@ -1,5 +1,14 @@
 package com.whileapp.springmvcrestangular.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AppController {
-    
+    @RequestMapping(value="/")
+    public String index(Model model){
+        model.addAttribute("name", "Prueba spring");
+        return "index";
+    }
 }
